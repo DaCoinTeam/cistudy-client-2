33840e1c43715419c5d50bacca7b9811cb369498
Nguyễn Văn Tự Cường <cuongnvtse160875@gmail.com>
2024-02-19 14:13:01 +0700

    123321

---
+++ src/app/courses/[id]/forum/_components/MiddleLayout/InfinitePostsScroller/PostCard/ContentBody/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { PostEntity } from "@common";
+import { ContentItem } from "./ContentItem";
+
+interface PostCardProps {
+  post: PostEntity;
+}
+
+export const ContentBody = (props: PostCardProps) => {
+  return (
+    <div className="flex flex-col gap-4 overflow-auto">
+      {props.post.postContents.map((postContent) => (
+        <ContentItem
+          key={postContent.postContentId}
+          postContent={postContent}
+        />
+      ))}
+    </div>
+  );
+};
--- src/app/courses/[id]/forum/_components/MiddleLayout/InfinitePostsScroller/PostCard/index.tsx
+++ src/app/courses/[id]/forum/_components/MiddleLayout/InfinitePostsScroller/PostCard/index.tsx
@@ -1,39 +1,80 @@
-import React from "react"
-import { PostEntity } from "@common"
-import { Card, CardBody, CardHeader, Divider, Spacer } from "@nextui-org/react"
-import { ContentItem } from "./ContentItem"
-import { CreatorAndStats } from "./CreatorAndStats"
-import Actions from "./Actions"
+import React, { createContext, useCallback, useMemo, useState } from "react";
+import { PostEntity, isErrorResponse } from "@common";
+import { Card, CardBody, CardHeader, Divider, Spacer } from "@nextui-org/react";
+import { CreatorAndStats } from "./CreatorAndStats";
+import Actions from "./Actions";
+import { findOnePost } from "@services";
+import {
+  PostCardState,
+  SetPostsAction,
+  usePostCardReducer,
+} from "./usePostCardReducer";
+import { ContentBody } from "./ContentBody";
 
 interface PostCardProps {
-    post: PostEntity
+  post: PostEntity;
 }
+
+interface PostCardContextValue {
+  state: PostCardState;
+  dispatch: React.Dispatch<SetPostsAction>;
+  functions: {
+    fetchAndSetPost: () => Promise<void>;
+  };
+}
+export const PostCardContext = createContext<PostCardContextValue | null>(null);
 
 export const PostCard = (props: PostCardProps) => {
-    const renderContents = () => (
-        <div className="flex flex-col gap-4 overflow-auto">
-            {props.post.postContents.map((content) => (
-                <ContentItem
-                    key={content.postContentId}
-                    postContent={content}
-                />
-            ))}
-        </div>
-    )
+  const [state, dispatch] = usePostCardReducer();
 
-    return (
-        <Card>
-            <CardHeader className="p-6 pb-4 font-semibold">
-                {props.post.title}
-            </CardHeader>
-            <Divider/>
-            <CardBody className="p-6">
-                {renderContents()}
-                <Spacer y={6}/>
-                <CreatorAndStats post={props.post}/>
-                <Spacer y={6}/>
-                <Actions post={props.post}/>
-            </CardBody>
-        </Card>
-    )
-}
+  const fetchAndSetPost = useCallback(async () => {
+    const response = await findOnePost(
+      {
+        postId: props.post.postId,
+      },
+      {
+        postReacts: {
+          liked: true,
+          userId: true,
+        },
+      }
+    );
+    if (!isErrorResponse(response)) {
+      dispatch({
+        type: "SET_REACT_POST_PARTIAL",
+        payload: response,
+      });
+    } else {
+      console.log(response);
+    }
+  }, []);
+
+  const postCardContextValue: PostCardContextValue = useMemo(
+    () => ({
+      state,
+      dispatch,
+      functions: {
+        fetchAndSetPost,
+      },
+    }),
+    [state]
+  );
+
+  return (
+    <PostCardContext.Provider value={postCardContextValue}>
+      <Card>
+        <CardHeader className="p-6 pb-4 font-semibold">
+          {props.post.title}
+        </CardHeader>
+        <Divider />
+        <CardBody className="p-6">
+          <ContentBody post={props.post} />
+          <Spacer y={6} />
+          <CreatorAndStats post={props.post} />
+          <Spacer y={6} />
+          <Actions post={props.post} />
+        </CardBody>
+      </Card>
+    </PostCardContext.Provider>
+  );
+};
