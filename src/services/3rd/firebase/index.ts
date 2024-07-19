a3b01f92d1e923b86c9ec0503a09c0b957e6c599
starci183 <cuongnvtse160875@gmail.com>
2024-07-19 21:29:47 +0700

    123232

--- src/services/3rd/firebase/index.ts
+++ src/services/3rd/firebase/index.ts
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
-import { getMessaging, getToken } from "firebase/messaging"
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
