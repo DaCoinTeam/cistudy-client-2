f152b0e48d25747aa4f29d6e888af84090a7c8b0
ZennyFZ <nhandinhdo291@gmail.com>
2024-07-22 17:05:13 +0700

    add quiz page

--- src/app/quiz/[id]/components/Countdown/index.tsx
+++ src/app/quiz/[id]/components/Countdown/index.tsx
@@ -1,27 +1,43 @@
 import React, { useState, useEffect, useContext} from "react"
-import { QuizContext } from "../../hooks"
+import { QuizContext, QuizProgressState, QuizTimeState } from "../../hooks"
 import { useRouter } from "next/navigation"
 import { useParams } from "next/navigation"
+import { ErrorResponse } from "@common"
+import { ToastType } from "../../../../_components"
+import { RootContext } from "../../../../_hooks"
 
 const useCountdown = (initialTime : number) => {
     const route = useRouter()
     const params = useParams()
     const lessonId = params.id as string
-    const {swrs, reducer} = useContext(QuizContext)!
-    const [state] = reducer
+    const {swrs} = useContext(QuizContext)!
+    const { notify } = useContext(RootContext)!
     const {finishQuizAttemptSwrMutation} = swrs
     const {trigger} = finishQuizAttemptSwrMutation
-    const timemilliseconds = Number(localStorage.getItem("quizTimeLimit")) * 60 * 1000 - Number(localStorage.getItem("quizRemainingTime"))
+    const quizProgressState: QuizProgressState = JSON.parse(localStorage.getItem("quizProgressState") as string)
+    const quizTimeState: QuizTimeState = JSON.parse(localStorage.getItem("quizTimeState") as string)
+    const timemilliseconds = Number(quizTimeState?.timeLimit) - Number(quizTimeState?.remainingTime)
     const [remainingTime, setRemainingTime] = useState(initialTime)
 
     useEffect(() => {
-        const intervalId = setInterval(() => {
+        const intervalId = setInterval(async() => {
             setRemainingTime((prevTime) => Math.max(prevTime - 1000, 0))
-            localStorage.setItem("quizRemainingTime", remainingTime.toString())
-            if (remainingTime === 0) {
-                trigger({data: {quizAttemptId: state.quizAttemptId, quizQuestionAnswerIds: state.quizQuestionAnswerIds, timeTaken: timemilliseconds}}).then(() => {
+            quizTimeState.remainingTime = remainingTime.toString()
+            localStorage.setItem("quizTimeState", JSON.stringify(quizTimeState))
+            if (Number(quizTimeState.remainingTime) === 0) {
+                try {
+                    await trigger({data: {quizAttemptId: quizProgressState.quizAttemptId, quizQuestionAnswerIds: quizProgressState.quizQuestionAnswerIds, timeTaken: timemilliseconds}})
                     route.push(`/lessons/${lessonId}`)
-                })
+                } catch (ex) {
+                    const { message } = ex as ErrorResponse
+                    notify!({
+                        data: {
+                            error: message as string
+                        },
+                        type: ToastType.Error
+                    })
+                    route.push(`/lessons/${lessonId}`)
+                }
             }
         }, 1000)
 
@@ -32,7 +48,7 @@ const useCountdown = (initialTime : number) => {
 }
 
 const CountdownTimer = ({ initialTime } : { initialTime : number}) => {
-    const convertedInitialTime = initialTime * 60 * 1000
+    const convertedInitialTime = initialTime
     const remainingTime = useCountdown(convertedInitialTime)
 
     const getTimeComponents = (time : number) => {
--- src/app/quiz/[id]/components/ResultModalRef/index.tsx
+++ src/app/quiz/[id]/components/ResultModalRef/index.tsx
@@ -1,7 +1,8 @@
 import { parseMillisecondsTime } from "@common"
 import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@nextui-org/react"
 import { X  } from "lucide-react"
-import { forwardRef, useImperativeHandle } from "react"
+import { forwardRef, useContext, useImperativeHandle } from "react"
+import { QuizContext } from "../../hooks"
 
 export interface ResultModalRefProps {
     onOKPress: () => void
@@ -15,7 +16,8 @@ export const ResultModalRef = forwardRef<
 ResultModalRefSelectors,
 ResultModalRefProps
 >((props, ref) => {
-    const timemilliseconds = Number(localStorage.getItem("quizTimeLimit")) * 60 * 1000 - Number(localStorage.getItem("quizRemainingTime"))
+    const {reducer} = useContext(QuizContext)!
+    const [state] = reducer
     const {onOKPress} = props
     const { isOpen, onOpen } = useDisclosure()
 
@@ -31,8 +33,8 @@ ResultModalRefProps
                         <ModalHeader className="p-4 pb-2 text-2xl">Your Result</ModalHeader>
                         <ModalBody className="p-4">
                             <div className="text-sm">
-                                <div className="font-semibold">Your score: {localStorage.getItem("quizScore")}/10</div>
-                                <div className="font-semibold">Your finish time: {parseMillisecondsTime(timemilliseconds)}</div>
+                                <div className="font-semibold">Your score: {state.score}/10</div>
+                                <div className="font-semibold">Your finish time: {parseMillisecondsTime(state.finishTime)}</div>
                             </div>
                         </ModalBody>
                         <ModalFooter className="p-4 pt-2">
