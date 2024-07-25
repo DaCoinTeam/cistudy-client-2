ce48e47b2902e5073878ac6eecb2530d4e2a4633
Thu Trang Le <lthutrang17@gmail.com>
2024-07-26 01:47:37 +0700

    fix cart layout

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,17 +1,17 @@
 "use client"
-import { Button, Checkbox, Divider, Spacer } from "@nextui-org/react"
-import React, { useContext, useMemo, useRef } from "react"
+import { XMarkIcon } from "@heroicons/react/24/outline"
+import { Button, Checkbox, Link, Spacer } from "@nextui-org/react"
+import { deleteFromCart } from "@services"
+import { useContext, useMemo, useRef } from "react"
+import { ToastType } from "../_components"
 import { RootContext } from "../_hooks"
-import { useCartReducer } from "./_hooks/useCartReducer"
 import {
     ConfirmDeleteModalRef,
     ConfirmDeleteModalRefSelectors,
-    CourseCardHorizontal,
 } from "../_shared"
-import { deleteFromCart } from "@services"
-import { ToastType } from "../_components"
-import { CheckIcon, TrashIcon } from "@heroicons/react/24/outline"
 import { CartItem } from "./_components/CartItem"
+import { useCartReducer } from "./_hooks/useCartReducer"
+import { ArrowRightIcon } from "@heroicons/react/24/solid"
 
 const Cart = () => {
     const [state, dispatch] = useCartReducer()
@@ -91,107 +91,170 @@ const Cart = () => {
         confirmDeleteModalRef.current?.onOpen()
 
     return (
-        <div className='relative h-full flex flex-col mx-auto max-w-[1080px] mt-16 '>
-            <div className='grid grid-cols-3 gap-40'>
-                <div className='col-span-2 flex flex-col mr-6 '>
-                    <div className='flex justify-between w-full'>
-                        <div className='flex flex-row items-center '>
-                            <Checkbox
-                                size='lg'
-                                color='secondary'
-                                isSelected={selectedItems.length === cartCourses?.length}
-                                onValueChange={handleDispatchSelectAll}
-                            />
-                            <div className='text-base text-slate-400'>Select All</div>
-                        </div>
-                        <Button
-                            isIconOnly
-                            variant='light'
-                            color='danger'
-                            onPress={onConfirmDeleteModalOpen}
-                            aria-label='Delete'
-                        >
-                            <TrashIcon className='w-5 h-5' />
-                        </Button>
-                    </div>
-                    <Spacer y={1} />
-                    <Divider />
-                    <Spacer y={4} />
-                    {cartCourses?.map((item) => (
-                        <div className='flex flex-row space-y-8' key={item.cartCourseId}>
-                            <div className='flex justify-center items-center rounded-lg'>
+        <div className='relative h-full flex flex-col mx-auto max-w-[1260px] mt-12 '>
+            <div className='text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl'>
+        Shopping Cart
+            </div>
+            <Spacer y={6} />
+
+            <div className='grid grid-cols-7 gap-6'>
+                <div className='mx-auto col-span-5 '>
+                    <div className='col-span-2 flex flex-col mr-6 '>
+                        <div className='flex items-center w-full justify-between px-4 py-2 rounded-lg border  border-gray-300 bg-white shadow-sm dark:border-gray-700 dark:bg-gray-800 '>
+                            <div className='flex items-center '>
                                 <Checkbox
                                     size='lg'
-                                    color='secondary'
-                                    isSelected={selectedItems.includes(item.cartCourseId)}
-                                    onValueChange={() => {
-                                        handleDispatchSelectItems(item.cartCourseId)
-                                    }}
+                                    color='primary'
+                                    classNames={
+                                        {
+                                            base: "border-slate-700 dark:border-slate-200"
+                                        }
+                                    }
+                                    isSelected={selectedItems.length === cartCourses?.length}
+                                    onValueChange={handleDispatchSelectAll}
+                                />
+                                <span className='text-slate-700 dark:text-slate-200'>All Products</span>
+                            </div>
+                            <div className='flex items-end'>
+                                <Button
+                                    variant='light'
+                                    color='danger'
+                                    onPress={onConfirmDeleteModalOpen}
+                                    startContent={<XMarkIcon className='w-5 h-5' />}
+                                    aria-label='Delete'
+                                >
+                  Remove
+                                </Button>
+                            </div>
+                        </div>
+                        <Spacer y={4} />
+                        <div className='space-y-4'>
+                            {cartCourses?.map((item) => (
+                                <div
+                                    className='flex flex-row p-4 py-6 rounded-lg border border-gray-300 bg-white  shadow-sm dark:border-gray-700 dark:bg-gray-800'
+                                    key={item.cartCourseId}
+                                >
+                                    <div className='flex justify-center items-center rounded-lg'>
+                                        <Checkbox
+                                            size='lg'
+                                            color='primary'
+                                            isSelected={selectedItems.includes(item.cartCourseId)}
+                                            onValueChange={() => {
+                                                handleDispatchSelectItems(item.cartCourseId)
+                                            }}
+                                        />
+                                    </div>
+                                    <CartItem {...item.course} />
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                <div className='mx-auto mt-5 col-span-2  space-y-6 lg:mt-0 lg:w-full'>
+                    <div className='space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 sm:p-6'>
+                        <div className='text-xl font-semibold text-gray-900 dark:text-white'>
+              Order summary
+                        </div>
+
+                        <div className='space-y-4'>
+                            <div className='space-y-2'>
+                                <dl className='flex items-center justify-between gap-4'>
+                                    <dt className='text-base font-normal text-gray-500 dark:text-gray-400'>
+                    Item{" "}
+                                    </dt>
+                                    <dd className='text-base font-medium text-gray-900 dark:text-white'>
+                                        {selectedItems.length}
+                                    </dd>
+                                </dl>
+
+                                <dl className='flex items-center justify-between gap-4'>
+                                    <dt className='text-base font-normal text-gray-500 dark:text-gray-400'>
+                    Original price
+                                    </dt>
+                                    <dd className='text-base font-medium text-gray-900 dark:text-white'>
+                                        {" "}
+                                        {getTotalPrice.price} STARCI
+                                    </dd>
+                                </dl>
+
+                                <dl className='flex items-center justify-between gap-4'>
+                                    <dt className='text-base font-normal text-gray-500 dark:text-gray-400'>
+                    Savings
+                                    </dt>
+                                    <dd className='text-base font-medium text-green-600'>
+                    -{getTotalPrice.price - getTotalPrice.discountPrice} STARCI
+                                    </dd>
+                                </dl>
+                            </div>
+
+                            <dl className='flex items-center justify-between gap-4 border-t border-gray-200 pt-2 dark:border-gray-700'>
+                                <dt className='text-base font-bold text-gray-900 dark:text-white'>
+                  Total
+                                </dt>
+                                <dd className='text-base font-bold text-gray-900 dark:text-white'>
+                                    {getTotalPrice.discountPrice} STARCI
+                                </dd>
+                            </dl>
+                        </div>
+
+                        <Button
+                            color='primary'
+                            disabled={selectedItems.length === 0}
+                            // onPress={()=> handleCheckout()}
+                            className='flex w-full items-center justify-center rounded-lg 700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
+                        >
+              Proceed to Checkout
+                        </Button>
+
+                        <div className='flex items-center justify-center gap-2'>
+                            <span className='text-sm font-normal text-gray-500 dark:text-gray-400'>
+                                {" "}
+                or{" "}
+                            </span>
+                            <Link
+                                title=''
+                                className='inline-flex items-center gap-2 text-sm font-medium text-primary-700 underline hover:no-underline dark:text-primary-500'
+                            >
+                Continue Shopping
+                                <ArrowRightIcon className="w-4 h-4"/>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className='space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 sm:p-6'>
+                        <form className='space-y-4'>
+                            <div>
+                                <label
+                                    htmlFor='voucher'
+                                    className='mb-2 block text-sm font-medium text-gray-900 dark:text-white'
+                                >
+                                    {" "}
+                  Do you have a voucher or gift card?{" "}
+                                </label>
+                                <input
+                                    type='text'
+                                    id='voucher'
+                                    className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500'
+                                    placeholder=''
+                                    required
                                 />
                             </div>
-                            <CartItem {...item.course} />
-                        </div>
-                    ))}
-                </div>
+                            <Button
+                                type='submit'
+                                className='flex w-full items-center justify-center rounded-lg bg-primary px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
+                            >
+                Apply Code
+                            </Button>
+                        </form>
+                    </div>
 
-                <div className='col-span-1 px-4 w-full  flex-col pt-4 pb-4 flex '>
-                    <div>
-                        <div className='text-3xl tracking-tight font-bold text-primary mb-6'>
-              Cart Summary
-                        </div>
-                        <div className='text-xl tracking-tight font-semibold mb-6 text-slate-500'>
-                            {selectedItems.length} items
-                        </div>
-                    </div>
-                    <Spacer y={4} />
-
-                    <Divider />
-                    <Spacer y={4} />
-
-                    <div className='w-full flex flex-row justify-between py-1'>
-                        <div className='text-lg tracking-tight font-semibold  mb-6'>
-              SubTotal
-                        </div>
-                        <div className='font-semibold text-primary leading-6'>
-                            {getTotalPrice.price} STARCI
-                        </div>
-                    </div>
-                    <div className='w-full flex flex-row justify-between pb-1'>
-                        <div className='text-lg tracking-tight font-semibold mb-6'>
-              Discount{" "}
-                        </div>
-                        <div className='font-semibold text-primary leading-6'>
-                            {getTotalPrice.price - getTotalPrice.discountPrice} STARCI
-                        </div>
-                    </div>
-                    <div className='w-full flex flex-row justify-between pb-1'>
-                        <div className='text-xl tracking-tight font-bold text-primary mb-6'>
-              Total{" "}
-                        </div>
-                        <div className='text-xl font-bold text-primary leading-6'>
-                            {getTotalPrice.discountPrice} STARCI
-                        </div>
-                    </div>
-                    <div className='w-full pt-2'>
-                        <Button
-                            color='secondary'
-                            className='py-1 w-full font-semibold'
-                            disabled={selectedItems.length === 0}
-                            startContent={<CheckIcon className='w-5 h-5' />}
-                            // onPress={()=> handleCheckout()}
-                        >
-              Checkout
-                        </Button>
-                    </div>
+                    <ConfirmDeleteModalRef
+                        ref={confirmDeleteModalRef}
+                        title='Delete item in cart ? '
+                        content='Are you sure delete item in cart'
+                        onDeletePress={handleDelete}
+                    />
                 </div>
             </div>
-
-            <ConfirmDeleteModalRef
-                ref={confirmDeleteModalRef}
-                title='Delete item in cart ? '
-                content='Are you sure delete item in cart'
-                onDeletePress={handleDelete}
-            />
         </div>
     )
 }
