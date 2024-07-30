98d072e7e3abb4e543515a19158fd1531a2a6399
starci183 <cuongnvtse160875@gmail.com>
2024-07-30 20:23:15 +0700

    123123123

--- src/app/courses/layout.tsx
+++ src/app/courses/layout.tsx
@@ -1,10 +1,15 @@
 "use client"
 
-import { ReactNode } from "react"
+import { ReactNode, Suspense } from "react"
 import { AllCoursesProvider } from "./_hooks"
+import { Spinner } from "@nextui-org/react"
 
 const Layout = ({ children }: { children: ReactNode }) => {
-    return <AllCoursesProvider>{children}</AllCoursesProvider>
+    return (
+        <Suspense fallback={<Spinner />}>
+            <AllCoursesProvider>{children}</AllCoursesProvider>
+        </Suspense>
+    )
 }
 
 export default Layout
